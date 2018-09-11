9abefe64388d88a3dbea188767cb07dd69bb293d
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-11 07:52:10 -0600

    Monsterapp, gitignore and created repo

---
+++ src/controller/MonstersController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class MonstersController
+{
+
+	public void start()
+	{
+		
+	}
+}
---
+++ src/controller/MonstersRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class MonstersRunner
+{
+
+	public static void main(String [] args)
+	{
+		MonstersController app = new MonstersController();
+		app.start();
+	}
+}
