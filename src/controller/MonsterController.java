17ac3b39507adcc0b3b5699d1275154278091f8b
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-14 08:28:10 -0600

    Added try catch methods at the bottom to catch errors put into the JOptionPane when you use the wrong value aka string to Int

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -15,21 +15,26 @@ public class MonsterController
 		//ring of death to go away.
 	private MarshmallowMonster userMonster;
 	//--------Constructor section---------
-	//public MonsterController()
+	public MonsterController()
 		//Still same name as class
 	{
 		myMonster = new MarshmallowMonster("Boog", 4.0, 3, true, 4);
 		Scanner userThings = new Scanner(System.in);
-		System.out.println("What is the name of your monster");
-		String a = userThings.nextLine();
-		System.out.println("How may legs?");
-		double b = userThings.nextDouble();
-		System.out.println("How many eyes?");
-		int c = userThings.nextInt();
-		System.out.println("Does it have a nose?");
-		boolean d = userThings.nextBoolean();
-		System.out.println("How many arms?");
-		int e = userThings.nextInt();
+		//System.out.println("What is the name of your monster");
+		String a = JOptionPane.showInputDialog(null,"What is the name of your monster?");
+		//String a = userThings.nextLine();
+		//System.out.println("How may legs?");
+		double b = JOptionPane.showInputDialog(null,"How may legs?");
+		//double b = userThings.nextDouble();
+		//System.out.println("How many eyes?");
+		int c = JOptionPane.showInputDialog(null,"How many eyes?");
+		//int c = userThings.nextInt();
+		//System.out.println("Does it have a nose?");
+		boolean d = JOptionPane.showInputDialog(null,"Does it have a nose?");
+		//boolean d = userThings.nextBoolean();
+		//System.out.println("How many arms?");
+		int e = JOptionPane.showInputDialog(null, "How many arms?");
+		//int e = userThings.nextInt();
 
 			//Job 1: initialize data members
 		userMonster = new MarshmallowMonster(a , b, c, d, e);
@@ -45,13 +50,47 @@ public class MonsterController
 		JOptionPane.showMessageDialog(null, myMonster);
 		// backslash n (\n) breaks the line on the java window
 		
-		//System.out.println(myMonster);
+		System.out.println(myMonster);
 		myMonster.setArmCount(9999);
+		
 		JOptionPane.showMessageDialog(null, "My Monster has" + myMonster.getArmCount() + " arms!");
-		//System.out.println("My monster has " + myMonster.getArmCount() + " arms!");
-
+		System.out.println("My monster has " + myMonster.getArmCount() + " arms!");
+		
+		JOptionPane.showMessageDialog(null, userMonster);
 		System.out.println(userMonster);
 		
 		
+	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You should type an actual number value!!!");
+			
+		}
+		return isValid;
+	}
+	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid= false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You should type a double value aka a decimal point boi");
+		}
+		return isValid;
 	}
 }
